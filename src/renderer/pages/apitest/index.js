dce0f3636c87d58ac31bd20ccabd3efb76767b22
lzuntalented <704540593@qq.com>
2018-02-28 23:36:51 +0800

    add api request method

--- src/renderer/pages/apitest/index.js
+++ src/renderer/pages/apitest/index.js
@@ -1,4 +1,7 @@
 const queryString = require('query-string');
+const http = require('http');
+const https = require('https');
+const Url = require('url');
 
 export default {
   name: 'apitest-page',
@@ -17,37 +20,118 @@ export default {
       reqestMethods,
       method: 'GET',
       url: '',
-      requestParamsTable: [{key:1,value:1},{key:2,value:2}],
-      requestHeaderTable: []
+      requestParamsTable: [{key: 'add',value: 'add'}],
+      requestHeaderTable: [{key: 'add',value: 'add'}],
+      responseTableColumns: [
+        {
+          key: '',
+          value: ''
+        }
+      ]
     }
   },
   methods: {
-    parse() {
-      let flag = '';
-      this.checkList.forEach(element => {
-        if(element === '1') {
-          flag += 'g';
-        } else if(element === '2'){
-          flag += 'i';
-        }
-      });
-      const regexString = this.regex || '';
-      const originText = this.originText || '';
-      const regex = new RegExp(regexString, flag);
-      this.result = originText.match(regex) || [];
-      this.resultLength = '匹配' + this.result.length + '处';
-    },
-    setRegex(str){
-      this.regex = str;
-    },
     arraySpanMethod({ row, column, rowIndex, columnIndex }) {
-      // console.log(rowIndex, columnIndex)
       if(rowIndex === this.requestParamsTable.length - 1 && columnIndex === 0) {
         return [1,3];
       }
       return [1,1]
     },
+    arrayHeaderSpanMethod({ row, column, rowIndex, columnIndex }) {
+      if(rowIndex === this.requestHeaderTable.length - 1 && columnIndex === 0) {
+        return [1,3];
+      }
+      return [1,1]
+    },
     renderOperateHeader(){
+    },
+    addParamRow(arr) {
+      arr.splice(arr.length - 1, 1);
+      arr.push({
+        key: '',
+        value: ''
+      });
+      arr.push({
+        key: 'add',
+        value: 'add'
+      });
+    },
+    delTableRow(arr, index){
+      arr.splice(index, 1);
+    },
+    sendRequest(){
+      // console.log(this.requestParamsTable);
+      // const u = 'http://www.lzuntalented.cn/sort/0?a=123&b=2';
+      // this.url = u;
+      const url = this.url.trim();
+      if (url === '') {
+        this.$message.error('请输入请求地址')
+        return;
+      }
+
+      const urlObject = Url.parse(url);
+      let httpHandler = http;
+
+      var options = {
+        hostname: urlObject.hostname,
+        port: urlObject.port || 80,
+        path: urlObject.pathname,
+        method: 'GET'
+      };
+
+      // 请求参数
+      let params = urlObject.query || '';
+
+      let tmp = [];
+      const pLen = this.requestParamsTable.length;
+      this.requestParamsTable.forEach((it, idx) => {
+        if (pLen - 1 === idx) return;
+        if (it.key.trim() === '') return;
+        tmp.push(it.key + '=' + it.value);
+      });
+      if (tmp.length > 0) {
+        params += '&' + tmp.join('&');
+      }
+
+      // console.log(params);
+
+      // https请求检测
+      if (url.startsWith('https://')) {
+        options.port = 443;
+        httpHandler = https;
+      }
+      
+      // post请求检测
+      if (this.method === 'POST') {
+        options.method = this.method;
+      }
+      // 开始请求
+      const req = httpHandler.request(options, res => {
+        // console.log(`STATUS: ${res.statusCode}`);
+        // console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        let html = '';
+        for (var i in res.headers) {
+          html += `<p>${i}：${res.headers[i]}</p>`;
+        }
+        this.responseTableColumns[0].key = html;
+
+        res.setEncoding('utf8');
+        let resBody = '';
+        res.on('data', (chunk) => {
+          resBody += chunk;
+        });
+        res.on('end', () => {
+          // console.log('No more data in response.')
+          this.responseTableColumns[0].value = resBody;
+        })
+      });
+
+      req.on('error', (e) => {
+        // this.$message.error(`problem with request: ${e.message}`);
+      });
+
+      req.write(params);
+      req.end();
     }
   }
 }
--- src/renderer/pages/apitest/indexPage.vue
+++ src/renderer/pages/apitest/indexPage.vue
@@ -1,6 +1,6 @@
 <template>
 	<el-card class="apitest-container box-card">
-		<div  class="url-area">
+		<div  class="url-area m-b-10">
 			<el-input placeholder="请输入请求地址" v-model="url" class="input-with-select" size="mini">
 				<el-select class="method" slot="prepend" v-model="method" placeholder="请选择" size="mini">
 						<el-option
@@ -10,12 +10,12 @@
 							:value="item.value">
 						</el-option>
 					</el-select>
-				<el-button slot="append" type="primary">
+				<el-button @click="sendRequest" slot="append" type="primary">
 					发起请求
 				</el-button>
 			</el-input>
 		</div>
-		<div>
+		<div class="m-b-10">
 			<el-table
 				:data="requestParamsTable"
 				:span-method="arraySpanMethod"
@@ -23,62 +23,133 @@
 				stripe
 				border
 				>
-				<el-button slot="empty" type="primary" size="mini">
-					添加
-				</el-button>
 				<el-table-column
 					prop="key"
 					label="Reqest参数名称"
 					>
 					<template slot-scope="scope">
 						<el-button
+							class="m-v display-block"
+							@click="addParamRow(requestParamsTable)"
+							type="primary" 
 							v-if="scope.$index === scope.store.states.data.length - 1"
 							size="mini">添加</el-button>
-							<div
-								class="text-center"
+							<el-input
+								size="mini"
+								v-model="scope.row.key"
 								v-if="scope.$index !== scope.store.states.data.length - 1"
-							>{{scope.row.key}}</div>
+								>
+							</el-input>
 					</template>
 				</el-table-column>
 				<el-table-column
 					prop="value"
 					label="Reqest参数值"
 					>
+					<template slot-scope="scope">
+						<el-input
+							size="mini"
+							v-model="scope.row.value"
+							>
+						</el-input>
+					</template>
 				</el-table-column>
 				<el-table-column
 					v-if="requestParamsTable.length > 0"
 					prop="value"
 					label="操作"
 					>
+					<template slot-scope="scope">
+						<el-button @click="delTableRow(requestParamsTable, scope.$index)" slot="empty" type="danger" size="mini">
+							删除
+						</el-button>
+					</template>
 				</el-table-column>
 			</el-table>
 		</div>
-		<div>
+		<div class="m-b-10">
 			<el-table
 				:data="requestHeaderTable"
+				:span-method="arrayHeaderSpanMethod"
 				size="mini"
 				stripe
 				border
 				>
-				<el-button slot="empty" type="primary" size="mini">
-					添加
-				</el-button>
 				<el-table-column
 					prop="key"
 					label="Header名称"
 					>
+					<template slot-scope="scope">
+						<el-button
+							class="m-v display-block"
+							@click="addParamRow(requestHeaderTable)"
+							type="primary" 
+							v-if="scope.$index === scope.store.states.data.length - 1"
+							size="mini">添加</el-button>
+							<el-input
+								size="mini"
+								v-model="scope.row.key"
+								v-if="scope.$index !== scope.store.states.data.length - 1"
+								>
+							</el-input>
+					</template>
 				</el-table-column>
 				<el-table-column
 					prop="value"
 					label="Header值"
 					>
+					<template slot-scope="scope">
+						<el-input
+							size="mini"
+							v-model="scope.row.value"
+							>
+						</el-input>
+					</template>
+				</el-table-column>
+				<el-table-column
+					v-if="requestHeaderTable.length > 0"
+					prop="value"
+					label="操作"
+					>
+					<template slot-scope="scope">
+						<el-button @click="delTableRow(requestHeaderTable, scope.$index)" slot="empty" type="danger" size="mini">
+							删除
+						</el-button>
+					</template>
 				</el-table-column>
 			</el-table>
 		</div>
-		<el-row>
-			<el-col>
-			</el-col>
-		</el-row>
+		<div class="m-b-10">
+			<el-table
+				:data="responseTableColumns"
+				size="mini"
+				stripe
+				border
+				>
+				<el-table-column
+					prop="key"
+					label="Response Header"
+					>
+					<template slot-scope="scope">
+						<div v-html="scope.row.key"></div>
+					</template>							
+				</el-table-column>
+				<el-table-column
+					prop="value"
+					label="Response Body"
+					>
+					<template slot-scope="scope">
+						<el-input
+							type="textarea"
+							:rows="8"
+							size="mini"
+							v-model="scope.row.value"
+							>
+						</el-input>
+					</template>
+				</el-table-column>
+			</el-table>
+		</div>
 	</el-card>
 </template>
 
@@ -95,6 +166,13 @@
 		}
 		.url {
 		}
+	}
+	.res-scroll {
+		height: 300px;
+		overflow: scroll;
+	}
+	p{
+		margin: 0 0;
 	}
 }
 
